c8ab4c216f9004723b342bc16900db6b55519a9a
schornkat <schornkat@gmail.com>
2022-06-20 00:52:53 +0200

    master

--- splife_frontend/src/client.js
+++ splife_frontend/src/client.js
@@ -1,7 +1,7 @@
-import SanityClient from "@sanity/client"
+import sanityClient from "@sanity/client"
 import ImageUrlBuilder from "@sanity/image-url"
 
-export const client = SanityClient({
+export const client = sanityClient({
   projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
   dataset: "production",
   apiVersion: "2022-06-14",
--- splife_frontend/src/components/Login.jsx
+++ splife_frontend/src/components/Login.jsx
@@ -3,7 +3,7 @@ import GoogleLogin from "react-google-login"
 import { useNavigate } from "react-router-dom"
 import { FcGoogle } from "react-icons/fc"
 import logi from "../assets/logi.png"
-import logo from "../assets/logowhite.png"
+import logoo from "../assets/logoo.png"
 
 import { client } from "../client"
 
@@ -29,11 +29,11 @@ const Login = () => {
   return (
     <div className="flex justify-start items-center flex-col h-screen">
       <div className=" relative w-full h-full">
-        <img src={logi} className="w-full h-full object-cover" />
+        <img src={logoo} className="w-full h-full object-cover" />
 
         <div className="absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0    bg-blackOverlay">
           <div className="p-5">
-            <img src={logo} width="130px" />
+            <img src={logg} width="250px" />
           </div>
 
           <div className="shadow-2xl">
