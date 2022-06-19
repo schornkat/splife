9ffecf644f368bc1b6e855a80e4149595274e7d4
schornkat <schornkat@gmail.com>
2022-06-19 12:24:08 +0200

    git add .

---
+++ splife_frontend/src/container/Home.jsx
@@ -0,0 +1,13 @@
+import React, { useState, useRef, useEffect } from "react"
+import { hiMenu } from "react-icons/hi"
+import { AiFillCloseCircle } from "react-icons/ai"
+import { Link, Route, Routes } from "react-router-dom"
+
+import Sidebar from "../components/Sidebar"
+import UserProfile from "../components/UserProfile"
+
+const Home = () => {
+  return <div>Home</div>
+}
+
+export default Home
